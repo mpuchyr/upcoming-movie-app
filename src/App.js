7fc3e59f83a31630e237ad813594e44a4c9985c9
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-06-10 15:06:41 -0500

    Create initial fetch of upcoming data in useEffect in src/App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React, { useEffect, useState } from 'react'; 
 import './App.css';
 
 function App() {
+  const [upcoming, setUpcoming] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    try {
+      fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_ACCESS_TOKEN}&language=en-US`)
+      .then(res => res.json())
+      .then(data => setUpcoming(data))
+    } catch {
+      setError('Something went wrong, please try again later')
+    }
+    setLoading(false)
+  }, [])
+
+  
   return (
     <div className="App">
       Main Page Placeholder
+      {loading && <h2>Loading data...</h2>}
+      {error && <p>{error}</p>}
     </div>
   );
 }
