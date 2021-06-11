5294c0c32b94144cfb318ceb039527a16607c668
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-06-11 10:38:54 -0500

    Import moment into components/UpcomingImages.js and change fetch address to fetch movies based on the date

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import UpcomingImages from './components/UpcomingImages';
+import moment from 'moment';
 import './App.css';
 
 function App() {
   const [upcoming, setUpcoming] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+  const [date, setDate] = useState(moment().format("YYYY-MM-DD"))
+
+  console.log(date)
+
 
   useEffect(() => {
     try {
-      fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_ACCESS_TOKEN}&language=en-US`)
+      // fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_ACCESS_TOKEN}&language=en-US&page=5`)
+      fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_ACCESS_TOKEN}&language=en-US&include_adult=false&include_video=false&primary_release_date.gte=${date}`)
       .then(res => res.json())
       .then(data => setUpcoming(data))
     } catch {
