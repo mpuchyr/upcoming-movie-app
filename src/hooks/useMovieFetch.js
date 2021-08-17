59071d1bb587db5adc3c0637c57c4912edbb209d
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-08-17 15:13:59 -0500

    Remove commented out code in hooks/useMovieFetch.js

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,9 +1,6 @@
 import { useEffect, useState } from 'react';
 
 const useMovieFetch = (date, setMovies, setLoading, setError) => {
-    // const [movies, setMovies] = useState(null)
-    // const [loading, setLoading] = useState(true)
-    // const [error, setError] = useState(null)
     const token = process.env.REACT_APP_TMDB_ACCESS_TOKEN
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${token}&language=en-US&include_adult=false&include_video=false&primary_release_date.gte=${date}`
 
@@ -17,20 +14,6 @@ const useMovieFetch = (date, setMovies, setLoading, setError) => {
         }
         setLoading(false)
     }, [date])
-
-
-    // return ({ movies, loading, error})
-
-    // useEffect( () => {
-    //     try {
-    //         fetch(url)
-    //         .then(res => res.json())
-    //         .then(data => setMovies(data))
-    //       } catch {
-    //         setError('Something went wrong, please try again later')
-    //       }
-    //       setLoading(false)    
-    // }, [])
 
 
 }
