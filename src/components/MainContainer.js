c8b9d6e26a46b1435d93991139db42c983fe1276
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-08-18 15:51:42 -0500

    Create components/MainContainer.js

---
+++ src/components/MainContainer.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import UpcomingImages from './UpcomingImages';
+import MovieInfo from './MovieInfo';
+import Footer from './Footer';
+
+const MainContainer = () => {
+
+}
+
+export default MainContainer
