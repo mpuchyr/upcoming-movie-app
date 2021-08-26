a6f8ecc6283557ec620ec424d0e2be42f8d491cb
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-08-26 14:18:10 -0500

    Remove commented out code from actions/actions.js

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -2,7 +2,6 @@ export const fetchMovies= (date, setMovies, setLoading, setError) => {
     const token = process.env.REACT_APP_TMDB_ACCESS_TOKEN
     const url = `https://api.themoviedb.org/3/discover/movie?api_key=${token}&language=en-US&include_adult=false&include_video=false&primary_release_date.gte=${date}`
 
-    // useEffect(() => {
         console.log('fetching movies')
         console.log(date)
         try {
@@ -13,7 +12,6 @@ export const fetchMovies= (date, setMovies, setLoading, setError) => {
             setError('Something went wrong, please try again later')
         }
         setLoading(false)
-    // }, [date])
 
 
 }
