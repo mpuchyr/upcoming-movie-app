6906f64f67a1ccc3e9cb9f78cd2c497d2849243d
mpuchyr <53955212+mpuchyr@users.noreply.github.com>
2021-08-31 16:06:44 -0500

    Add movieDate function in components/MovieInfo.js to show the movie release date in a specific format

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import moment from 'moment';
 
 const MovieInfo = ({movie}) => {
     const url = "https://image.tmdb.org/t/p/w500"
+
+    const movieDate = (date) => {
+        let movieDate = moment(date).format('LL')
+        return <h2>{movieDate}</h2>
+    }
 
     return (
         <div className="movie-info-container">
             <img src={`${url}/${movie.poster_path}`} alt="movie poster"/>
             <div className="movie-info">
                 <h1>{movie.original_title}</h1>
-                <h2>{movie.release_date}</h2>
+                {movieDate(movie.release_date)}
                 <p>{movie.overview}</p>
             </div>
         </div>
